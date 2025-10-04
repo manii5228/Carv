b770db73f6420d881a8c683037675bd5c3fc4318
manii5228 <manimanjunath.v@gmail.com>
2025-10-04 18:16:46 +0530

    Add files via upload
    parts of frontend

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+// no script needed yet
+</script>
+
+<style>
+/* Apply Biscotti font */
+@font-face {
+  font-family: 'Biscotti';
+  src: url('./assets/biscotti.ttf') format('truetype');
+}
+body {
+  margin: 0;
+  font-family: 'Biscotti', sans-serif;
+  background: #f9f9f9;
+}
+</style>
